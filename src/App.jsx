fee06e7b4e9b9b546c2bf098c9169d9b33b4085e
agu.banchi <au.banchi@gmail.comgit config --global user.email agu.banchi@gmail.com>
2026-04-20 10:38:23 -0300

    Terminando paginación, mostramos de 5 resultados por página

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import JobListings from "./components/JobListings"
 import Pagination from "./components/Pagination"
 import SearchFormSection from "./components/SearchFormSection"
 import jobsData from './data.json'
+
+const RESULTS_PER_PAGE = 5
+
 function App() {
 
   const [currentPage, setCurrentPage] =useState(1)
-  const totalPages = 5
-
+  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE)
+const pageResults = jobsData.slice(
+  currentPage -1 * RESULTS_PER_PAGE, // Página 1 -> 0, Página 2 -> 5, Página 3 -> 10
+  currentPage * RESULTS_PER_PAGE // Página 1 -> 5 Página 2 -> 10, Página 3 -> 15
+)
   const handlePageChange = (page)=>{
-    console.log('cambiando pagina...', page);
     setCurrentPage(page)
   }
 
@@ -21,7 +26,7 @@ function App() {
 
   <main>
 <SearchFormSection/>
-<JobListings jobs={jobsData}/>
+<JobListings jobs={pageResults}/>
   </main>
 
 <Pagination
