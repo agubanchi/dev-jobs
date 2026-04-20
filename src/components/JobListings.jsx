76e751e967d84cd54d99f943b79122d6eb82d39c
agu.banchi <au.banchi@gmail.comgit config --global user.email agu.banchi@gmail.com>
2026-04-20 10:14:58 -0300

    renderizando las Jobscard en mi componente de JobListing mediante map, tomamos los campos de mi archivo data para mostrarlos dinamicamente

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import jobsData from '../data.json'
+import JobCard from './Jobcard'
+
 
 export default function JobListings() {
   return (
     <>
     <h2>Resultados de búsqueda</h2>
         <div className="jobs-listings">
-          
+          {jobsData.map(job=>(
+            <JobCard key={job.id} job={job}/>
+          ))}
         </div>
         </>
   )
--- src/components/Jobcard.jsx
+++ src/components/Jobcard.jsx
@@ -1,22 +1,22 @@
-import data from '../data.json'
-
-console.log(data);
 
 
-export default function Jobcard() {
+
+export default function JobCard({job}) {
   return (
-    <article data-modalidad="remoto" className="job-listing-card">
-    {data.map}
+    <article
+    className="job-listing-card"
+    data-modalidad={job.data.modalidad}
+    data-nivel={job.data.nivel}
+    data-technology={job.data.technology}
+  >
     <div>
-      <h3>Ingeniero de Software</h3>
-      <small>Tech Solutions Inc. | Remoto</small>
-      <p>
-        Buscamos un ingeniero de software con experiencia en desarrollo web y conocimientos en
-        JavaScript, React y Node.js. El candidato ideal debe ser capaz de trabajar en equipo y tener
-        buenas habilidades de comunicación.
-      </p>
+      <h3>{job.titulo}</h3>
+      <small>
+        {job.empresa} | {job.ubicacion}
+      </small>
+      <p>{job.descripcion}</p>
     </div>
-    <button className="button-apply-job" id="boton-importante">Aplicar</button>
+    <button>Aplicar</button>
   </article>
   )
 }
